de8b8a9ae15c5d240d3242a6df800c6f81678981
--replace-all <akhbharti@gmail.com>
2019-07-10 01:07:00 +0530

    Updates

---
+++ precache-manifest.383351963cb3848c55fcc2dc0d048441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667d2011e9b6548a77d888f4295e69f1",
+    "url": "/niftywindow-assignment/index.html"
+  },
+  {
+    "revision": "4180398f789077d3a1a5",
+    "url": "/niftywindow-assignment/static/css/2.1ef35742.chunk.css"
+  },
+  {
+    "revision": "4156088469810dcd84c9",
+    "url": "/niftywindow-assignment/static/css/main.81c4812f.chunk.css"
+  },
+  {
+    "revision": "4180398f789077d3a1a5",
+    "url": "/niftywindow-assignment/static/js/2.8ad6565d.chunk.js"
+  },
+  {
+    "revision": "4156088469810dcd84c9",
+    "url": "/niftywindow-assignment/static/js/main.bd8ec55d.chunk.js"
+  },
+  {
+    "revision": "11ed9c6678888dafbecf",
+    "url": "/niftywindow-assignment/static/js/runtime~main.3869207d.js"
+  }
+]);
